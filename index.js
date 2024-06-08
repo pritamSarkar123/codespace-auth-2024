2e49c743dc6916012ef25ddaf055d17a162fed97
Pritam Sarkar <41255644+pritamSarkar123@users.noreply.github.com>
2024-06-08 17:06:36 +0000

    first home route created

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("Hello");
+const app = require("./app");
+
+
+const {PORT} = process.env
+app.listen(PORT, ()=>{
+    console.log(`server is running at port: ${PORT}`);
+});
+
+app.get("/",(req,res)=>{
+    res.send("<h1>Welcome to my API</h1>");
+})
