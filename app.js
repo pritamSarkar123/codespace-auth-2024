92c96bc02f364e107eb594f1e5b79490de4f4845
Pritam Sarkar <41255644+pritamSarkar123@users.noreply.github.com>
2024-06-08 18:28:53 +0000

    register user

--- app.js
+++ app.js
@@ -3,5 +3,6 @@ require("./database/database").connect()
 const express = require('express');
 
 const app = express();
+app.use(express.json());
 
 module.exports = app;
--- index.js
+++ index.js
@@ -9,3 +9,15 @@ app.listen(PORT, ()=>{
 app.get("/",(req,res)=>{
     res.send("<h1>Welcome to my API</h1>");
 })
+app.post("/register", async (req,res)=>{
+    try{
+        // get data from body
+        // all the data should exists
+        // if user already exists
+        // encrypt the password 
+        // save the user in db
+        // generate the token for user and send it
+    } catch (error) {
+        console.log(error);
+    }
+})
